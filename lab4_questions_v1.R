0c5081683cd53780a931530099644bbdd6d5246c
Evangelos Kassos <ekassos@college.harvard.edu>
2022-03-01 00:10:29 -0500

    Final

--- lab4_questions_v1.R
+++ lab4_questions_v1.R
@@ -13,6 +13,7 @@ if (!require(haven)) install.packages("haven"); library(haven)
 if (!require(sandwich)) install.packages("sandwich"); library(sandwich)
 if (!require(lmtest)) install.packages("lmtest"); library(lmtest)
 if (!require(tidyverse)) install.packages("tidyverse"); library(tidyverse)
+if (!require(stargazer)) install.packages("stargazer"); library("stargazer")
 
 #Methods/concepts: treatment effect estimation in stratified experiments, bar graphs,
 #multivariable regression, statistical inference, statistical vs. practical significance
@@ -36,7 +37,11 @@ if (!require(tidyverse)) install.packages("tidyverse"); library(tidyverse)
 
 #2.	Using the star.dta file, how does average class size (class_size) compare in small kindergarten 
 #classes vs. regular kindergarten classes (small == 1 vs. small == 0)?
-  
+
+df <- read_dta("star.dta")
+class_size_small <- with(df, mean(class_size[small == 1]))
+class_size_regular <- with(df, mean(class_size[small == 0]))
+
 #3.	At the end of kindergarten school year, students took four Stanford Achievement Tests: Math-
 #SAT math, Reading-SAT read, Word-SAT wordskill, and Listening-SAT listen.  It is common in 
 #education research to convert test scores into more meaningful units.  One way is to generate 
@@ -49,12 +54,25 @@ if (!require(tidyverse)) install.packages("tidyverse"); library(tidyverse)
 #divided by control_sd(math score), where control_mean(math score) and control_sd(math 
 #score) are calculated for observations with small == 0.
 
+df$std_math <- (df$math - (with(df, mean(math[small == 0])))) /(with(df, sd(math[small == 0])))
+df$std_read <- (df$read - (with(df, mean(read[small == 0])))) /(with(df, sd(read[small == 0])))
+df$std_wordskill <- (df$wordskill - (with(df, mean(wordskill[small == 0])))) /(with(df, sd(wordskill[small == 0])))
+df$std_listen <- (df$listen - (with(df, mean(listen[small == 0])))) /(with(df, sd(listen[small == 0])))
+
 #b.	Then generate sat_index as the average of these four standardized exam scores.  Some 
 #pseudo code is:  sat_index = mean(standardized math score, standardized reading 
 #score, standardized word score, standardized listening score)
 
+df$sat_index <- (df$std_math + df$std_read + df$std_wordskill + df$std_listen) / 4
+
 #c.	Plot a histogram of sat_index for small kindergarten classes (small == 1) and for 
 #regular kindergarten classes (small == 0).  What do you notice in the histograms?
+
+g1 <- ggplot(df, aes(x=sat_index, 
+                 fill=factor(small, labels=c("Regular class", "Small class")), 
+                 y=..density..)) +
+  geom_histogram(alpha=.5, position="identity", bins = 40) +
+  labs(x = "Average of four standardized exam scores", fill = "Class Size")
 
 #4.	Returning to question 1, we will assess whether the data are consistent with teachers having 
 #been randomly assigned to classrooms by testing for balance of teacher characteristics.  The 
@@ -67,12 +85,22 @@ if (!require(tidyverse)) install.packages("tidyverse"); library(tidyverse)
 #teacher_black, teacher_experience as well as the mean of sat_index across all the 
 #students in the teacher's class (which we'll use in question 5).
 
+by_teacher <- df %>% group_by(teacher_id, small, school_id, teacher_masters, teacher_white, teacher_black, teacher_experience)  %>% select(small, school_id, teacher_id, teacher_masters, teacher_white, teacher_black, teacher_experience, sat_index) 
+                     
+classes <- by_teacher %>% summarise(sat_index = mean(sat_index)) 
+                            
 #b.	Estimate a linear regression (lm in R or regress in stata) of teacher_experience on an 
 #intercept and small.  Use the estimated coefficient on small to report the difference in 
 #average teacher experience in small vs. large classes.  Calculate a 95% confidence 
 #interval for this difference: Estimated Difference \pm 1.96 \times standard error.
 
+teacher_experience_small <- lm(teacher_experience ~ small, data = df)
+
 #c.	Repeat question b for teacher_masters, teacher_white, and teacher_black.
+
+teacher_experience_masters <- lm(teacher_experience ~ teacher_masters, data = df)
+teacher_experience_white <- lm(teacher_experience ~ teacher_white, data = df)
+teacher_experience_black <- lm(teacher_experience ~ teacher_black, data = df)
 
 #d.	Are the differences in teacher characteristics in small vs. large classes statistically 
 #significantly different from zero?  Are they practically significant?  What do you 
@@ -94,6 +122,8 @@ if (!require(tidyverse)) install.packages("tidyverse"); library(tidyverse)
 #sat_index on the small class indicator small, controlling for school fixed effects (e.g., 
 #regress with i.school_id in Stata; or lm with factor(school_id) in R).  
 
+teacher_experience_small_factor <- lm(sat_index ~ small + factor(school_id), data = df)
+
 #b.	Use the estimated coefficient on the small class indicator small to report your estimate 
 #of the causal effect of class size from this regression.   Calculate a 95% confidence 
 #interval for this difference: Estimated Difference \pm 1.96 \times standard error.
@@ -104,6 +134,39 @@ if (!require(tidyverse)) install.packages("tidyverse"); library(tidyverse)
 #height of the bar for the treatment group should equal the sum of the control group 
 #mean and regression coefficient on small from the regression in part a.  Add square 
 #brackets to the treatment group bar to visualize the 95% confidence interval from part b.
+
+mean_sat_index <- with(df, mean(sat_index[small == 0]))
+treatment_group_average <- mean_sat_index + teacher_experience_small_factor$coefficients[2]
+
+data_for_graph <- data.frame(c(mean_sat_index, treatment_group_average), 
+                             c(NA, 0.023), 
+                             c("Control group", "Treatment group"))  
+data_for_graph
+
+# Change name of 1st column of data_for_graph to "Moved"
+names(data_for_graph)[1] <- "Experience"
+
+# Change name of 2nd column of data_for_graph to "se"
+names(data_for_graph)[2] <- "se"
+
+# Change name of 2nd column of data_for_graph to "Group"
+names(data_for_graph)[3] <- "Group"
+
+#Add upper bound on 95% CI
+data_for_graph$ub <- data_for_graph$Experience + 1.96*data_for_graph$se
+
+#Add lower bound on 95% CI
+data_for_graph$lb <- data_for_graph$Experience - 1.96*data_for_graph$se
+
+#Look at the data frame we have created
+data_for_graph
+
+# Bar graph displaying results
+g2 <- ggplot(data=data_for_graph, aes(x=Group, y=Experience, fill=Group)) +
+  geom_bar(stat="identity", show.legend = FALSE, width=.6) +
+  geom_errorbar(aes(ymin=lb, ymax=ub), width=.1, size = 0.7, color="black") +
+  scale_fill_manual(values=c("red", "blue")) +
+  labs(y = "Average of four standardized exam scores\n")
 
 #6.	For this Lab, please submit the following:
 #a.	Your do-file or .R script file to Gradescope
